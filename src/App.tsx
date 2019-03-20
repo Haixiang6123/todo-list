192f50ae64a81c9044ec5a53e5b12444c2270aa8
Haixiang <haixiang6123@gmail.com>
2019-03-20 11:15:07 -0700

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { Button, Rate } from 'antd';
 import * as React from 'react';
+import {BrowserRouter as Router, Link, Route} from "react-router-dom"
+import Index from './components/Index/Index'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 import './App.scss';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <Rate character="6"/>
-        <Button>Hello</Button>
-      </div>
-    );
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+        <Route exac={true} path="/" component={Index}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+      </Router>
+    )
   }
 }
 
