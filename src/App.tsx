3a9d146efa142fd22770b58cd5709a80663a82a2
Haixiang6123 <haixiang6123@gmail.com>
2019-03-20 00:29:17 -0700

    Try Ant Design Lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { Rate } from 'antd';
+import { Button, Rate } from 'antd';
 import * as React from 'react';
 import './App.scss';
-
-import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <Rate character="6"/>
+        <Button>Hello</Button>
       </div>
     );
   }
