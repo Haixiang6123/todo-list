cdb54becd98b9ccc09455d9d589955cca9758eec
Haixiang <haixiang6123@gmail.com>
2019-03-21 18:39:18 -0700

    Add antd Input

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,22 +1,49 @@
 import * as React from 'react'
-import { Button } from 'antd'
+import { Button, Input, Icon } from 'antd'
 
-class Component extends React.Component {
+interface IProps {
+
+};
+interface IState {
+  account: string
+  confirmPassword: string
+  password: string
+};
+
+class Register extends React.Component<IProps, IState> {
   constructor(props) {
     super(props)
+    this.state = {
+      account: '',
+      confirmPassword: '',
+      password: ''
+    }
   }
 
-  public render() {
-    return (
-      <div className="register">
-        <Button onClick={this.register} htmlType="button">Register</Button>
-      </div>
-    )
+  private onChangeAccount = (e) => {
+    this.setState({
+      account: e.target.value
+    })
   }
 
   private register = () => {
     console.log('register')
   }
+
+  public render() {
+    const { account } = this.state
+    return (
+      <div className="register">
+        <Input
+          prefix={<Icon type="user" style={{color: 'rgba(0, 0, 0, 0.25)'}}/>}
+          placeholder="Account"
+          value={account}
+          onChange={this.onChangeAccount}
+        />
+        <Button onClick={this.register} htmlType="button">Register</Button>
+      </div>
+    )
+  }
 }
 
-export default Component
+export default Register
